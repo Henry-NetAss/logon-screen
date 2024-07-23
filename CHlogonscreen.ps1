78797ab826c0afa411a5d47fc3af67277855ea3b
Henry-NetAss <142622280+Henry-NetAss@users.noreply.github.com>
2024-07-23 10:32:29 +0200

    Add files via upload

---
+++ CHlogonscreen.ps1
@@ -0,0 +1,6 @@
+
+$Key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP'
+if (!(Test-Path -Path $Key)) {
+   New-Item -Path $Key -Force | Out-Null
+}
+Set-ItemProperty -Path $Key -Name LockScreenImagePath -value "C:\na\Installation\chemchamplogonlogo.jpg"
